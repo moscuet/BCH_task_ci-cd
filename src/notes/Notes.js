35fb9278fff10fb0369556b117c42ce56299e98b
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-08-13 05:21:08 +0300

    testing ci/cd

---
+++ src/notes/Notes.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+const Notes = () => {
+  const [note, setNote] = useState('');
+  const [notes, setNotes] = useState([]);
+
+
+  const taskDone =notes.filter( note => note.active===false).length
+
+  const handleChange  = (event) => {
+       setNote(event.target.value)
+  }
+
+  const handleAdd  = (event) => {
+      if(!note)return
+    let newNote = {
+        text:note,
+        active:true,
+        id:notes.length+1
+       }
+      setNotes(notes.concat(newNote))
+      setNote('')
+      console.log(notes)
+}
+const toggleActive = (id) =>{
+  const modifiedNotes = notes.map( note=>{
+      if(note.id===id) return {text:note.text, active:!note.active,id:note.id}
+      return note
+  })
+  setNotes(modifiedNotes)
+}
+
+const handleDel =(id) =>{
+    const modifiedNotes =[]
+    notes.forEach( note=>{
+        if(note.id!==id)  modifiedNotes.push({text:note.text, active:note.active,id:modifiedNotes.length+1})
+    })
+    setNotes([...modifiedNotes])
+}
+
+
+ let lists = notes.map( (note) => (
+        <li key={note.id} > 
+            <p onClick ={ ()=>toggleActive(note.id)} className = {note.active?'':'inactive-text'} >{note.text}</p>  
+            <button onClick = {() =>handleDel(note.id)}>delet</button>
+        </li>
+    
+    ))
+  return (
+    <div>
+        <div>
+            <p>Add new note</p>
+            <input type="text" value={note} onChange = {handleChange}></input> <button onClick = {handleAdd} >add</button>
+            <p>{note}</p>
+        </div>
+        <p> { notes.length>0 &&taskDone===notes.length? 'wow! mission accomplished': `Total task:${notes.length}, completed task ${taskDone}`}</p>
+      <h3> Notes</h3>
+      <ol>{lists}</ol>
+
+    </div>
+  );
+}
+
+export default Notes
